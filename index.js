f604a36726cc107b539ce549a27ca8e1720d3d49
Ayesha khan <112780976+ayeshoooo@users.noreply.github.com>
2023-10-04 19:43:50 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+//practice ex 1.3
+
+
+console.log("My name is Ayesha");
+
+//practice ex 1.4
+
+let a = 20;
+console.log(a);
+
+// let b = 20;
+// console.log(a);
+
+// let c = 20;
+// console.log(a);
+
+// let d = 20;
+// console.log(a);
+// console.log(Math.random()*500);
+
+
+
---
+++ newFile.js
@@ -0,0 +1,21 @@
+
+//Self check quiz
+
+//Question 1
+
+//What is the HTML syntax to add an external JavaScript file?
+ //We can use the script tag with the attribute src.
+
+ //Question 2
+
+ //Can you run JavaScript in a file with a JS extension in your browser?
+
+ //To execute JavaScript in a browser you have two options — either put it inside a script element anywhere inside an HTML document, or put it inside an external JavaScript file (with a . js extension) and then reference that file inside the HTML document using an empty script element with a src attribute.
+
+ //Question 3
+ //How do you write a multiple-line comment in JavaScript?
+
+ //writing the comment between a forward-slash followed by asterisk /* and asterisk followed by a forward-slash */ .
+
+// What is the best way to remove a line of code from running that you might
+//want to keep as you debug?
